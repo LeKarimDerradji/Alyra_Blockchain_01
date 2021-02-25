4ac9026f6b14cdf5eaa8afc64917344f196bb5ab
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-02-25 20:56:53 +0100

    chalk integration

--- programming/exercices-expressions-conditions/elevator.js
+++ programming/exercices-expressions-conditions/elevator.js
@@ -1,11 +1,15 @@
+const chalk = require('chalk')
+
 let currentFloor = 0
-let targetFloor = 6
+let targetFloor = 1
+
+console.log(chalk.blue('Bievenue, '));
 
 if (currentFloor !== targetFloor && currentFloor > 0) {
     console.log('Veuillez selectionner un étage...')
     switch (targetFloor) {
         case 1 :
-            console.log(`Vous êtes à l'étage numero ${currentFloor} et vous allez au première étage`)
+            console.log(chalk`Vous êtes à l'étage numero {blue.bold ${currentFloor}} et vous allez au première étage`)
             break;
         case 2 :
         case 3 :
@@ -13,19 +17,19 @@ if (currentFloor !== targetFloor && currentFloor > 0) {
         case 5 :
         case 6 :
         case 7 :
-            console.log(`Vous êtes à l'étage numero ${currentFloor} et vous allez à l'étage numéro ${targetFloor}`)
+            console.log(chalk.yellow`Vous êtes à l'étage numero {blue.bold ${currentFloor}} et vous allez à l'étage numéro {green.bold ${targetFloor}}`)
             break;
         case -1:
-            console.log(`Vous êtes à l'étage numero ${currentFloor} et vous descendez au sous-sol de premier niveau`)
+            console.log(chalk.yellow`Vous êtes à l'étage numero {blue.bold ${currentFloor}} et vous descendez au sous-sol de premier niveau`)
             break;
         case -2:
-            console.log(`Vous êtes à l'étage numero ${currentFloor} et vous descendez au sous-sol de deuxième niveau`)
+            console.log(chalk.yellow`Vous êtes à l'étage numero {blue.bold ${currentFloor}} et vous descendez au sous-sol de deuxième niveau`)
             break;
     } 
 }       else if (currentFloor !== targetFloor && currentFloor === 0) {
         switch (targetFloor) {
             case 1 :
-                console.log(`Vous êtes au rez de chaussez et vous allez au première étage`)
+                console.log(chalk.greenBright`Vous êtes au {cyanBright.bold rez de chaussez} et vous allez au {yellow.bold première étage}`)
                 break;
             case 2 :
             case 3 :
@@ -33,19 +37,19 @@ if (currentFloor !== targetFloor && currentFloor > 0) {
             case 5 :
             case 6 :
             case 7 :
-                console.log(`Vous êtes au rez de chaussez et vous allez à l'étage numéro ${targetFloor}`)
+                console.log(`Vous êtes au {cyanBright.bold rez de chaussez} et vous allez à l'étage numéro ${targetFloor}`)
                 break;
             case -1:
-                console.log(`Vous êtes au rez de chaussez et vous descendez au sous-sol de premier niveau`)
+                console.log(chalk`Vous êtes au {cyanBright.bold rez de chaussez} et vous descendez au sous-sol de premier niveau`)
                 break;
             case -2:
-                console.log(`Vous êtes au rez de chaussez et vous descendez au sous-sol de deuxième niveau`)
+                console.log(chalk`Vous êtes au {cyanBright.bold rez de chaussez} et vous descendez au sous-sol de deuxième niveau`)
                 break;
     }
 }   else if (currentFloor !== targetFloor && targetFloor === 0) {
     switch (currentFloor) {
         case 1 :
-            console.log(`Vous êtes au première étage et vous allez au rez de chaussez`)
+            console.log(chalk`Vous êtes au première étage et vous allez au {cyanBright.bold rez de chaussez}`)
             break;
         case 2 :
         case 3 :
@@ -53,13 +57,13 @@ if (currentFloor !== targetFloor && currentFloor > 0) {
         case 5 :
         case 6 :
         case 7 :
-            console.log(`Vous êtes à l'étage numéro ${currentFloor} et vous allez au rez de chaussez`)
+            console.log(chalk`Vous êtes à l'étage numéro ${currentFloor} et vous allez {cyanBright.bold rez de chaussez}`)
             break;
         case -1:
-            console.log(`Vous êtes à l'étage numéro ${currentFloor}  et vous descendez au sous-sol de premier niveau`)
+            console.log(chalk`Vous êtes à l'étage numéro ${currentFloor}  et vous descendez au sous-sol de premier niveau`)
             break;
         case -2:
-            console.log(`Vous êtes à l'étage numéro ${currentFloor} et vous descendez au sous-sol de deuxième niveau`)
+            console.log(chalk`Vous êtes à l'étage numéro ${currentFloor} et vous descendez au sous-sol de deuxième niveau`)
             break;
 }
 
